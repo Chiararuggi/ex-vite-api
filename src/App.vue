e3a6a0388a198b1d86076cb154fc7a43ed59bef9
chiara.ruggi <chiara.ruggi1998@gmail.com>
2023-11-30 15:26:26 +0100

    create appcard

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script>
+import AppCard from './components/AppCard.vue'
 import axios from 'axios';
 import { store } from "./store.js"
 export default {
   components: {
+    AppCard
   },
   data() {
     return {
+		store,
     }
   },
   mounted() {
