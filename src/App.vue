69b7b5dba832e5efec6dd70cf6ca7ef45a77599f
chiara.ruggi <chiara.ruggi1998@gmail.com>
2023-11-30 15:16:58 +0100

    start

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import axios from 'axios';
+import { store } from "./store.js"
+export default {
+  components: {
+  },
+  data() {
+    return {
+    }
+  },
+  mounted() {
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<template></template>
+
+<style scoped></style>
